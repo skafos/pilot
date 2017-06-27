ae850c7854709f0c3850e9baf63d638194e7878c
David Sison <david@metismachine.com>
2017-06-27 13:05:19 -0400

    Provide a way to pass response headers
    * NOTE: This is a breaking change!
    
    * Future-proof against addition of new `opts`
      - Altered definitions to hold up better against 
        future updates by placing all options into a
        Keyword list that can be parsed as support for
        new functionality grows
    * Updated tests to match new function definitions
    * Response functions that set headers internally
      will override any headers already on the conn or
      passed in as parameters, preventing subversion
      of a response function’s purpose.

--- lib/responses.ex
+++ lib/responses.ex
@@ -16,15 +16,24 @@ defmodule Pilot.Responses do
   Sends a JSON response
 
   ## Parameters
-  - conn:   HTTP connection to send response to.
-  - status: Atom or number that represents HTTP response code.
-  - data:   Struct to be encoded to JSON as the response body.
+  - conn: HTTP connection to send response to.
+  - data: Struct to be encoded to JSON as the response body.
+  - opts: Keyword list of options, supports
+          - status: Atom or number that represents HTTP response code.
+          - resp_headers: Map or list of 2-tuple response headers
+                          to be added to the response
 
   ## Example
-      conn |> json(:ok, %{hello: "World"})
+      conn |> json(%{hello: "World"})
   """
-  def json(conn, status, data) do
+  def json(conn, data, opts \\ []) do
+    status = Keyword.get(opts, :status, :ok)
+    resp_headers = Keyword.get(opts, :resp_headers, [])
+    json(conn, data, status, resp_headers)
+  end
+  defp json(conn, data, status, resp_headers) do
     conn
+    |> Plug.Conn.merge_resp_headers(resp_headers)
     |> Plug.Conn.put_resp_content_type("application/json")
     |> Plug.Conn.send_resp(status, Poison.encode_to_iodata!(data))
     |> Plug.Conn.halt()
@@ -34,15 +43,24 @@ defmodule Pilot.Responses do
   Sends a HTML response
 
   ## Parameters
-  - conn:   HTTP connection to send response to.
-  - status: Atom or number that represents HTTP response code.
-  - data:   HTML string set as the response body.
+  - conn: HTTP connection to send response to.
+  - data: HTML string set as the response body.
+  - opts: Keyword list of options, supports
+          - status: Atom or number that represents HTTP response code.
+          - resp_headers: Map or list of 2-tuple response headers
+                          to be added to the response
 
   ## Example
-      conn |> html(:ok, "<h1>Hello World</h1>")
+      conn |> html("<h1>Hello World</h1>")
   """
-  def html(conn, status, data) do
+  def html(conn, data, opts \\ []) do
+    status = Keyword.get(opts, :status, :ok)
+    resp_headers = Keyword.get(opts, :resp_headers, [])
+    do_html(conn, data, status, resp_headers)
+  end
+  defp do_html(conn, data, status, resp_headers) do
     conn
+    |> Plug.Conn.merge_resp_headers(resp_headers)
     |> Plug.Conn.put_resp_content_type("text/html")
     |> Plug.Conn.send_resp(status, to_string(data))
     |> Plug.Conn.halt()
@@ -52,15 +70,24 @@ defmodule Pilot.Responses do
   Sends a HTML response
 
   ## Parameters
-  - conn:   HTTP connection to send response to.
-  - status: Atom or number that represents HTTP response code.
-  - data:   Text string set as the response body.
+  - conn: HTTP connection to send response to.
+  - data: Text string set as the response body.
+  - opts: Keyword list of options, supports
+          - status: Atom or number that represents HTTP response code.
+          - resp_headers: Map or list of 2-tuple response headers
+                          to be added to the response
 
   ## Example
-      conn |> text(:ok, "Hello World!")
+      conn |> text("Hello World!")
   """
-  def text(conn, status, data) do
+  def text(conn, data, opts \\ []) do
+    status = Keyword.get(opts, :status, :ok)
+    resp_headers = Keyword.get(opts, :resp_headers, [])
+    do_text(conn, data, status, resp_headers)
+  end
+  defp do_text(conn, data, status, resp_headers) do
     conn
+    |> Plug.Conn.merge_resp_headers(resp_headers)
     |> Plug.Conn.put_resp_content_type("text/plain")
     |> Plug.Conn.send_resp(status, to_string(data))
     |> Plug.Conn.halt()
@@ -72,12 +99,20 @@ defmodule Pilot.Responses do
   ## Parameters
   - conn:   HTTP connection to send response to.
   - status: Atom or number that represents HTTP response code.
+  - opts:   Keyword list of options, supports
+            - resp_headers: Map or list of 2-tuple response headers
+                            to be added to the response
 
   ## Example
       conn |> status(:ok)
   """
-  def status(conn, status) do
+  def status(conn, status, opts \\ []) do
+    resp_headers = Keyword.get(opts, :resp_headers, [])
+    do_status(conn, status, resp_headers)
+  end
+  defp do_status(conn, status, resp_headers) do
     conn
+    |> Plug.Conn.merge_resp_headers(resp_headers)
     |> Plug.Conn.send_resp(status, "")
     |> Plug.Conn.halt()
   end
@@ -86,15 +121,24 @@ defmodule Pilot.Responses do
   Redirects request.
 
   ## Parameters
-  - conn:   HTTP connection to redirect.
-  - url:    String representing URL/URI destination.
-  - status: Atom or number that represents HTTP response code.
+  - conn: HTTP connection to redirect.
+  - url:  String representing URL/URI destination.
+  - opts: Keyword list of options, supports
+          - status: Atom or number that represents HTTP response code.
+          - resp_headers: Map or list of 2-tuple response headers
+                          to be added to the response
 
   ## Example
       conn |> redirect("http://<foo>.com/")
   """
-  def redirect(conn, url, status \\ 302) do
+  def redirect(conn, url, opts \\ []) do
+    status = Keyword.get(opts, :status, 302)
+    resp_headers = Keyword.get(opts, :resp_headers, [])
+    do_redirect(conn, url, status, resp_headers)
+  end
+  defp do_redirect(conn, url, status, resp_headers) do
     conn
+    |> Plug.Conn.merge_resp_headers(resp_headers)
     |> Plug.Conn.put_resp_header("location", url)
     |> Plug.Conn.send_resp(status, "")
     |> Plug.Conn.halt()
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,2 +1,1 @@
 ExUnit.start()
-Application.put_env(:pilot, :auth_secret, "super-secure-auth-key")
